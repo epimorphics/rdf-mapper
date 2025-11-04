82684a38a1219ee25f875e57b02fe728f4cd5663
Kal Ahmed <kal.ahmed@epimorphics.com>
2025-11-04 14:35:45 +0000

    Treat an empty string value as an unmapped value

--- src/rdf_mapper/lib/template_support.py
+++ src/rdf_mapper/lib/template_support.py
@@ -76,7 +76,7 @@ def valueof_var(var: str, state: TemplateState) -> Any:
                 val = fn(val, state)
         else:
             raise ValueError(f"Could not find function {fnname}")
-    if val is None:
+    if val is None or val == "":
         raise ValueError(f"could not find value for '{varname}'")
     return val
 
